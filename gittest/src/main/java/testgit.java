71926de8962df19bb2da06b898c3c727cb176e19
qianyejiu <2827588479@qq.com>
2020-10-31 14:49:39 +0800

    第二次测试github

--- gittest/src/main/java/testgit.java
+++ gittest/src/main/java/testgit.java
@@ -1,5 +1,7 @@
 public class testgit {
     public static void main(String[] args) {
         System.out.println("测试git上传");
+        System.out.println("ahah");
+        System.out.println("第二次测试github");
     }
 }
